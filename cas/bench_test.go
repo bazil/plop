dcbff5d6a15c84b59aae642fdc8db97a56409f29
Tommi Virtanen <tv@eagain.net>
2020-12-22 14:13:26 -0700

    cas: Add benchmark for Read

--- cas/bench_test.go
+++ cas/bench_test.go
@@ -3,6 +3,8 @@ package cas_test
 import (
 	"bytes"
 	"context"
+	"io"
+	"io/ioutil"
 	"testing"
 
 	"bazil.org/plop/cas"
@@ -41,6 +43,56 @@ func BenchmarkCreate(b *testing.B) {
 	run := func(name string, size int) {
 		b.Run(name, func(b *testing.B) {
 			benchmarkCreate(b, size)
+		})
+	}
+	run("1 KiB", 1*1024)
+	run("10 KiB", 10*1024)
+	run("100 KiB", 100*1024)
+	run("1 MiB", 1*1024*1024)
+	run("10 MiB", 10*1024*1024)
+}
+
+func benchmarkRead(b *testing.B, size int) {
+	b.ReportAllocs()
+	b.SetBytes(int64(size))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	randR := NewRandReader(1337)
+	buf := make([]byte, size)
+	randR.Read(buf)
+
+	bucket := memblob.OpenBucket(nil)
+	s := cas.NewStore("s3kr1t", cas.WithBucket(bucket))
+	key, err := s.Create(ctx, bytes.NewReader(buf))
+	if err != nil {
+		b.Fatalf("create failed: %v", err)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			h, err := s.Open(ctx, key)
+			if err != nil {
+				b.Fatalf("open failed: %v", err)
+			}
+			r := h.IO(ctx)
+			n, err := io.Copy(ioutil.Discard, r)
+			if err != nil {
+				b.Fatalf("read failed: %v", err)
+			}
+			if g, e := n, int64(size); g != e {
+				b.Errorf("wrong read size: %v != %v", g, e)
+			}
+		}
+	})
+}
+
+func BenchmarkRead(b *testing.B) {
+	run := func(name string, size int) {
+		b.Run(name, func(b *testing.B) {
+			benchmarkRead(b, size)
 		})
 	}
 	run("1 KiB", 1*1024)
